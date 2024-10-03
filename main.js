06ea448fd0ef9b86c866ec62cb3098dd24c3f161
abuhoraira153 <abuhoraira153@gmail.com>
2024-10-03 21:52:00 +0600

    js tasks added

---
+++ main.js
@@ -0,0 +1,45 @@
+let sarasApple = 25;
+let sarasBuyingApple = 15;
+let sarasTotalApple = sarasApple + sarasBuyingApple;
+console.log("Total of Sara's apples are " + sarasTotalApple);
+
+let jhonsMoney = 500;
+let jhonsDebitMoney = 200;
+let jhonsRemainingMoney = jhonsMoney - jhonsDebitMoney;
+console.log("Jhon's remaing is " + jhonsRemainingMoney + " taka .");
+
+let makeMachenePerHour = 120;
+let totalHours = 8;
+let totalMachines = makeMachenePerHour * totalHours;
+console.log("Total machins are " + totalMachines);
+
+let treesPerRow = 12;
+let totalRows = 8;
+let remainingTrees = 20;
+let totalTrees = treesPerRow * totalRows + remainingTrees;
+console.log("Total trres are " + totalTrees);
+
+let totalBoxes = 5;
+let orangesPerBox = 10;
+let gaveFriend = 12;
+let remainingOranges = totalBoxes * orangesPerBox - gaveFriend;
+console.log("Rahim's remaining oranges are " + remainingOranges);
+
+let totalDistance = 150;
+let totalTime = 3;
+let distancePerHour = totalDistance / totalTime;
+console.log("Distance per hour is " + distancePerHour + " km .");
+
+let factoryMachene = 6;
+let makeProductsPerDay = 200;
+factoryMachene += 2;
+let totalMakeProducts = factoryMachene * makeProductsPerDay;
+let unsoldProducts = 3;
+let remainingProducts = totalMakeProducts - unsoldProducts;
+console.log("Total make products are " + totalMakeProducts);
+console.log("Remaining products are " + remainingProducts);
+
+let totalChocolates = 120;
+let chocolatesPerPacket = 9;
+let remainingChocolates = totalChocolates % chocolatesPerPacket;
+console.log("Remaining chocolates are " + remainingChocolates);
